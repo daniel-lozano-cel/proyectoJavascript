b43c92b458299d6efa760f7f09f767a2f44a3356
Daniel Lozano Cely <danielozano435@gmail.com>
2022-10-03 13:12:13 -0500

    added a new .js file with fetch to the proyect

---
+++ js/fetch.js
@@ -0,0 +1,14 @@
+fetch('https://fakestoreapi.com/products/1', {
+    method: 'POST',
+    headers: {
+        'Content-Type':'application/json'
+    },
+    body: JSON.stringify({
+        name: "User 1"
+    })
+}).then(res=>{
+    return res.json()
+})
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
+    
